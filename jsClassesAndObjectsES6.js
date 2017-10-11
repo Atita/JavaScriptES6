24fd9b0ad0574fc84f8bce606a5a2ce63c4574c2
Atita <anaguasoft@gmail.com>
2017-10-11 15:39:44 +0300

    added node.js module readline usage to jsClassesAndObject.js

--- jsClassesAndObjectsES6.js
+++ jsClassesAndObjectsES6.js
@@ -1,5 +1,16 @@
-//ES2015 CLASSES - ES6
+/*ES2015 CLASSES - ES6
+with node.js readline()
+*/
+
 "uses strict";
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 
 class laptop {
 
@@ -30,10 +41,16 @@ class laptop {
 }
 
 var hp = new laptop("hp", 512, 13.3);
+//console.log(hp);
 hp.getModel();
 hp.setModel("Acer");
 
 var xt = new laptop("xt", 256, 15.0);
 xt.getMemory();
-hp.setMemory(1024);
+rl.question('how much memory do you need? ', (answer) => {
+  // TODO: Log the answer in a database
+  console.log(`Setting up memory with size : ${answer}`);
+  hp.setMemory(answer);
+  rl.close();
+});
 xt.getMemory();
